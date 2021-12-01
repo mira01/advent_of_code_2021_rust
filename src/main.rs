a396e24788f3e6285c7b2d8e757c4436a5916baf
Miroslav Čech <miroslav.cech@gooddata.com>
2021-12-01 09:03:28 +0100

    day_1_1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{stdin, BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = BufReader::new(stdin());
+    let result = compute(&mut stream);
+    println!("{}", result);
+}
+
+fn compute<T: BufRead>(mut input: &mut T) -> usize{
+   let (sum, _) = input.lines().map(|x|{
+       x.unwrap().parse::<usize>().unwrap()
+    }).fold((0, None), |(sum, prev), current|{
+       if let Some(prev) = prev{
+        if current > prev{
+            return (sum + 1, Some(current))
+        }
+        else {
+            return (sum, Some(current))
+        }
+       }
+    return (0, Some(current))
+   });
+   sum
+}
+
+#[test]
+fn test_input(){
+   let mut input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263".as_bytes();
+   assert_eq!(7, compute(&mut input));
 }
