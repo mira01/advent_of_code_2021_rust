89e173c8b1fd00afb80ff077ba69736833e7412b
Miroslav Čech <miroslav.cech@gooddata.com>
2021-12-01 20:23:48 +0100

    day_1_2

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,29 @@ use std::io::{stdin, BufRead, BufReader};
 fn main() {
     let mut stream = BufReader::new(stdin());
     let result = compute(&mut stream);
-    println!("{}", result);
+    println!("result: {:?}", result);
 }
-
 fn compute<T: BufRead>(mut input: &mut T) -> usize{
-   let (sum, _) = input.lines().map(|x|{
-       x.unwrap().parse::<usize>().unwrap()
-    }).fold((0, None), |(sum, prev), current|{
-       if let Some(prev) = prev{
-        if current > prev{
-            return (sum + 1, Some(current))
-        }
-        else {
-            return (sum, Some(current))
-        }
-       }
-    return (0, Some(current))
-   });
-   sum
+    let numbers: Vec<usize> = input.lines().map(|x|{
+        x.unwrap().parse::<usize>().unwrap()
+    }).collect();
+    let numbers_slice = &numbers[..];
+    let (count, _) = numbers_slice.windows(3).fold((0, None), |(count, prev), current|{
+        let sum = current[0] + current[1] + current[2];
+        if let Some(prev) = prev{
+            if sum > prev {
+                (count + 1, Some(sum))
+            } else {
+                (count, Some(sum))
+            }
+        } else {(0, Some(sum))}
+    });
+    count
 }
 
 #[test]
 fn test_input(){
-   let mut input = "199
+    let mut input = "199
 200
 208
 210
@@ -35,5 +35,5 @@ fn test_input(){
 269
 260
 263".as_bytes();
-   assert_eq!(7, compute(&mut input));
+    assert_eq!(5, compute(&mut input));
 }
