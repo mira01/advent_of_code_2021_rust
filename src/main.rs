505d2e801ec32bdf4c10c24d4d2f5738f52ccc0d
Miroslav Čech <miroslav.cech@gooddata.com>
2021-12-02 16:49:35 +0100

    day_2_2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{stdin, BufRead, BufReader};
+use std::ops::Add;
+
 fn main() {
     let mut stream = BufReader::new(stdin());
     let result = compute(&mut stream);
@@ -15,15 +18,16 @@ enum Direction{
 struct Position{
     depth: usize,
     forward: usize,
+    aim: usize,
 }
 
 impl Add<Direction> for Position{
     type Output = Position;
     fn add(self, other: Direction) -> Self{
         match other{
-            Direction::Forward(n) => Position{depth: self.depth, forward: self.forward + n},
-            Direction::Down(n) => Position{depth: self.depth + n, forward: self.forward},
-            Direction::Up(n) => Position{depth: self.depth - n, forward: self.forward},
+            Direction::Forward(n) => Position{depth: self.depth + (self.aim * n), forward: self.forward + n, aim: self.aim},
+            Direction::Down(n) => Position{depth: self.depth, forward: self.forward, aim: self.aim + n},
+            Direction::Up(n) => Position{depth: self.depth, forward: self.forward, aim: self.aim - n},
         }
     }
 }
@@ -41,10 +45,10 @@ fn parse(line: &str) -> Direction{
 }
 
 fn compute<T: BufRead>(mut input: &mut T) -> usize{
-    let Position{depth, forward} = input.lines()
+    let Position{depth, forward, aim} = input.lines()
         .map(|x|{ parse(&x.unwrap()) }
         )
-        .fold(Position{depth:0, forward:0}, |acc, current|{
+        .fold(Position{depth:0, forward:0, aim: 0}, |acc, current|{
             acc + current
         });
     depth * forward
@@ -58,5 +62,5 @@ forward 8
 up 3
 down 8
 forward 2".as_bytes();
-   assert_eq!(150, compute(&mut input));
+   assert_eq!(900, compute(&mut input));
 }
